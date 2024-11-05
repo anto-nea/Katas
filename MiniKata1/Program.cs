9789418d437e8f161a093d936992415a30b9bcd8
anto-nea <antonia.tomus@gmail.com>
2024-11-05 15:00:37 +0100

    add minikata1
    variables

---
+++ MiniKata1/Program.cs
@@ -0,0 +1,11 @@
+﻿string characterName = "Arin";
+int healthPoints = 100;
+float attackPower = 15.5f;
+bool isParalyzed = true;
+double currencyAmount = 24.75;
+
+Console.WriteLine($"Character Name: {characterName}");
+Console.WriteLine($"Health Points: {healthPoints}");
+Console.WriteLine($"Attack Power: {attackPower}");
+Console.WriteLine($"Is Paralyzed: {isParalyzed}");
+Console.WriteLine($"Gold Coins: {currencyAmount}");
