dc5a72464bf308c039e8a6ea6c23a951589fc94e
anto-nea <antonia.tomus@gmail.com>
2024-11-10 18:04:25 +0100

    add variables & data types

---
+++ Kata2/Program.cs
@@ -0,0 +1,18 @@
+﻿string characterName = "Lira";
+int level = 5;
+float healthPoints = 75.5f;
+double experiencePoints = 1025.75;
+bool hasMagicAbility = true;
+char rank = 'A';
+
+double nextLevelThreshold = 1200.0;
+
+double pointsToNextLevel = nextLevelThreshold - experiencePoints;
+
+Console.WriteLine($"Character Name: {characterName}");
+Console.WriteLine($"Level: {level}");
+Console.WriteLine($"Health Points: {healthPoints}");
+Console.WriteLine($"Experience Points: {experiencePoints}");
+Console.WriteLine($"Has Magic Ability: {hasMagicAbility}");
+Console.WriteLine($"Rank: {rank}");
+Console.WriteLine($"Points needed to reach next level: {pointsToNextLevel}");
