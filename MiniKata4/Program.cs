a051edc927b425ac03528552917fce82524960d3
anto-nea <antonia.tomus@gmail.com>
2024-11-10 18:32:25 +0100

    add basic loops

---
+++ MiniKata4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Spawning enemies using a for loop:");
+    int numberOfEnemiesForLoop = 5;
+
+    for (int i = 1; i <= numberOfEnemiesForLoop; i++)
+        {
+            Console.WriteLine($"Enemy #{i} spawned!");
+        }
+
+Console.WriteLine("\nSpawning enemies using a while loop:");
+    int numberOfEnemiesWhileLoop = 3;
+    int count = 1;
+
+    while (count <= numberOfEnemiesWhileLoop)
+        {
+            Console.WriteLine($"Enemy #{count} spawned!");
+        count++;
+        }
