91237cb57e0dcef0631e7e3f3b71292b2e476236
anto-nea <antonia.tomus@gmail.com>
2024-11-12 11:51:57 +0100

    add simple methods

---
+++ MiniKata5/Program.cs
@@ -0,0 +1,21 @@
+﻿class GameActions
+{
+//attack
+static void Attack(int damage)
+    {
+        Console.WriteLine($"Player dealt {damage} damage!");
+    }
+
+//healing
+    static void Heal(int healAmount)
+    {
+        Console.WriteLine($"Player healed {healAmount} health points!");
+    }
+
+//both
+    static void Main(string[] args)
+    {
+        Attack(15);
+        Heal(10);
+    }
+}
