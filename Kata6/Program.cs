a11ca976d695a80928a47a65585830640f320b25
anto-nea <antonia.tomus@gmail.com>
2024-11-17 20:01:07 +0100

    arrays + lists frfr

---
+++ Kata6/Program.cs
@@ -0,0 +1,38 @@
+﻿class Game
+{
+    static void Main(string[] args)
+    {
+        //array enemy anmes
+        string[] enemies = { "Goblin", "Orc", "Troll", "Skeleton", "Dragon" };
+        Console.WriteLine("Enemies:");
+        foreach (string enemy in enemies)
+        {
+            Console.WriteLine(enemy);
+        }
+
+        // list of inventory items
+        var inventory = new List<string> { "Sword", "Shield", "Potion" };
+        Console.WriteLine("\nPlayer Inventory:");
+        foreach (string item in inventory)
+        {
+            Console.WriteLine(item);
+        }
+
+        // add items
+        inventory.Add("Helmet");
+        inventory.Add("Armor");
+
+        // remove item
+        inventory.Remove("Potion");
+
+        // update list
+        Console.WriteLine("\nUpdated Inventory:");
+        foreach (string item in inventory)
+        {
+            Console.WriteLine(item);
+        }
+
+        // display no. total items
+        Console.WriteLine("\nTotal Items in Inventory: " + inventory.Count);
+    }
+}
