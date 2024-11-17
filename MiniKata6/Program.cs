f506768d5a8751c26d31d7b266a5abc126c31e5f
anto-nea <antonia.tomus@gmail.com>
2024-11-17 19:49:50 +0100

    add arrays + lists

---
+++ MiniKata6/Program.cs
@@ -0,0 +1,36 @@
+﻿class Game
+{
+    static void Main(string[] args)
+    {
+    //arrays for enemies
+        string[] enemies = { "Goblin", "Orc", "Troll" };
+        Print("Enemies:");
+        foreach (string enemy in enemies)
+            {
+            Print(enemy);
+            }
+    
+    // list of inventory
+        var inventory = new List<string> { "Sword", "Shield", "Potion" };
+        Print("\nPlayer Inventory:");
+        foreach (string item in inventory)
+            {
+            Print(item);
+            }
+    
+    //add another item on list + display
+        inventory.Add("Helmet");
+        Print("\nUpdated Inventory:");
+        foreach (string item in inventory)
+            {
+            Print(item);
+            }
+    }
+
+static void Print(string message)
+    {
+    Console.WriteLine(message);
+    }
+}
+
+
